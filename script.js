1446f65b71b733413431003eb99710b4fcc18f68
Git0302 <115480548+Git0302@users.noreply.github.com>
2023-07-06 08:49:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// getting the html elements
+const decrementBtn = document.getElementById("decrementBtn");
+const incrementBtn = document.getElementById("incrementBtn");
+const resetBtn = document.getElementById("resetBtn");
+const displayValue = document.getElementById("displayValue");
+
+// for decrement button click
+decrementBtn.addEventListener("click", decrementFun);
+
+function decrementFun()
+{
+    const value = Number(displayValue.innerText);
+    if (value > 0) {
+      displayValue.innerText = value - 1;
+    } else {
+      alert("Negative value not allowed");
+    } 
+}
+
+
+// for increment button click
+incrementBtn.addEventListener("click",incrementFun);
+
+function incrementFun()
+{
+  const value = Number(displayValue.innerText);
+  if(value >= 10) 
+  {
+    alert("10+ values are not allowed");
+  } 
+  else 
+  {
+    displayValue.innerText = value + 1;
+  }
+}
+
+// for reset button click
+resetBtn.addEventListener("click", resetFun); 
+
+function resetFun()
+{
+  displayValue.innerText = 0;
+}
